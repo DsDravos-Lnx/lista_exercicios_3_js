ec3b6e291a813643977ce01f20bf8d661afdf2d3
DsDravos <ds.hallucinate@gmail.com>
2019-12-12 01:06:12 -0300

    Ex07

---
+++ Ex07/js/script.js
@@ -0,0 +1,11 @@
+const tamanhoNome = function(){
+    
+    let nome = prompt('digite seu nome completo:')    
+    letras = nome.replace(/\s/g, '')
+    
+    alert(`seu nome possui ${letras.length} letras`)
+    alert(`seu nome possui ${nome.length} caracteres`)
+    
+}
+
+tamanhoNome()

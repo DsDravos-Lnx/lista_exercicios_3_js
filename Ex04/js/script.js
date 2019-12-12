98d633abb998dd9346e1d785adde6490ea50c6b2
DsDravos <ds.hallucinate@gmail.com>
2019-12-12 00:38:24 -0300

    Ex04

---
+++ Ex04/js/script.js
@@ -0,0 +1,21 @@
+const inverterNomes = function(){
+    let laco = true, maiores = 0, menores = 0
+
+    while (laco == true){
+        let idade = prompt('digite uma idade:')
+        
+        if (idade.toUpperCase() == 'FIM'){
+            laco = false
+        } else if  (Number(idade) >= 18) {
+            maiores += 1
+        } else {
+            menores += 1
+        }
+    }
+
+    alert(`${maiores} pessoas sao maiores de idade!`)
+    alert(`${menores} pessoas sao menores de idade!`)
+
+}
+
+inverterNomes()
